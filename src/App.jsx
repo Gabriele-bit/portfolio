edac5db73562384fc5de1e2979fcf9fd23a16f27
Gabriele Patanè <pataneg17@gmail.com>
2025-07-15 13:38:03 +0200

    migliorie

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,28 @@ function App() {
     <>
       {/*Container padre*/}
       <div className='flex flex-1 flex-col overflow-visible'>
-        {/*Container 1 */}  
-        <div className='flex flex-1 flex-row min-h-screen'> 
+        {/*Container 1 */}
+        <div className='flex flex-1 flex-row min-h-screen'>
           {/*Container testo */}
-          <div className='bg-[#AEC6CF] flex-2/3 flex-col items-center content-center'>
+          <div className='bg-[#AEC6CF] flex flex-2/3 flex-col items-center content-center'>
+
             {/*testo */}
-            <div className='w-[30em]  flex flex-col justify-center justify-self-center items-center'>
+            <div className='w-[30em flex flex-1 flex-col justify-center justify-self-center items-center'>
               <p className='text-white  text-[2rem] font-[bungee] '>Hi! </p>
               <p className='text-[#444444]  font-[bungee] text-[3rem]  '>Gabriele Patanè </p>
               <p className='text-white  text-[2rem] font-[bungee]   '>here. </p>
-              <div className='w-[100%] bg-amber-50'>
-                <HashLink smooth to="#aboutme">
-                  About me
-                </HashLink>
-                <HashLink smooth to="#projects">
-                  About me
-                </HashLink>
-                <HashLink smooth to="#contact">
-                  About me
-                </HashLink>
-              </div>
+            </div>
+            
+            <div className='w-[100%] flex p-[20px] font-[prompt] justify-end flex-0.5 text-white gap-2'>
+              <HashLink smooth to="#aboutme" className='bg-[#444444] flex-1 text-center'>
+                About me
+              </HashLink>
+              <HashLink smooth to="#projects" className='bg-[#444444] flex-1 text-center '>
+                My Projects
+              </HashLink>
+              <HashLink smooth to="#contact" className='bg-[#444444] flex-1 text-center'>
+                Contact & Resume
+              </HashLink>
             </div>
           </div>
           <div className='bg-[#444444] max-w-[33.3em] flex-1/3 flex justify-center items-center '>
@@ -46,7 +48,7 @@ function App() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis pariatur, quibusdam tempora fugiat soluta expedita dolores vel repellat repudiandae enim ipsum alias? Iure deserunt corporis consequuntur dolores suscipit qui sapiente repudiandae facere quisquam laborum impedit, obcaecati quibusdam. Nostrum officiis ad molestias fugiat facere nemo. Vitae facere nesciunt saepe a obcaecati ducimus, quasi voluptatum, laborum sed similique ipsam incidunt illum nemo neque cumque sequi repudiandae soluta quam voluptas porro molestias officia laudantium? Doloribus nostrum laborum esse quo voluptatum, exercitationem dignissimos delectus porro magnam quia laudantium illum architecto magni quasi dolore aperiam temporibus, quibusdam voluptatibus saepe rerum tempora eius. Consequatur, neque est.
         </div>
         <br></br>
-        <div id='contact'className='min-h-screen'>
+        <div id='contact' className='min-h-screen'>
           ciao
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis pariatur, quibusdam tempora fugiat soluta expedita dolores vel repellat repudiandae enim ipsum alias? Iure deserunt corporis consequuntur dolores suscipit qui sapiente repudiandae facere quisquam laborum impedit, obcaecati quibusdam. Nostrum officiis ad molestias fugiat facere nemo. Vitae facere nesciunt saepe a obcaecati ducimus, quasi voluptatum, laborum sed similique ipsam incidunt illum nemo neque cumque sequi repudiandae soluta quam voluptas porro molestias officia laudantium? Doloribus nostrum laborum esse quo voluptatum, exercitationem dignissimos delectus porro magnam quia laudantium illum architecto magni quasi dolore aperiam temporibus, quibusdam voluptatibus saepe rerum tempora eius. Consequatur, neque est.
         </div>
