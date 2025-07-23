180eeca8fd481f98cb4aad01c1f8c0209c9cf520
Gabriele Patanè <pataneg17@gmail.com>
2025-07-23 14:55:04 +0200

    migliorie grafiche

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,26 @@ function App() {
   return (
     <div className='flex flex-col w-[100%] shadow-xl shadow-[#C9A74D] h-full overflow-y-auto scrollable '>
       <div className=' w-[100%] text-white flex justify-center self-center gap-[5vw] font-sans rounded-tl-0 rounded-bl-[2vh]
-      rounded-tr-0 rounded-br-[2vh] p-[2vh] text-center lg:w-[70%] lg:p-[1vh] bg-red-400'>
+      rounded-tr-0 rounded-br-[2vh]  text-center lg:w-[70%] p-2 bg-red-400'>
         <HashLink smooth to="#aboutme" className='text-[3vh]  transition-transform hover:scale-130 h-fit hover:text-black hover:rounded-[2vh] hover:font-bold cursor-pointer'>
-          About me
+          About
         </HashLink>
         <HashLink smooth to="#projects" className=' text-[3vh] transition-transform  hover:scale-130 h-fit hover:text-black hover:rounded-[2vh] hover:font-bold cursor-pointer'>
-          My Projects
+          Projects
         </HashLink>
         <HashLink smooth to="#contact" className='  text-[3vh] transition-transform  hover:scale-130 h-fit hover:text-black hover:rounded-[2vh] hover:font-bold cursoor-pointer'>
-          Contact me
+          Contacts
         </HashLink>
       </div>
       {/* <Header></Header>
-      <About></About>
-      <Contact></Contact> */}
+      <About></About> */}
       <Alternative></Alternative>
       <div className=' p-5 flex justify-center w-[100%] h-[100%] items-center'><hr className=' p-[0.5px] w-[70%]  bg-red-100'></hr></div>
       <About></About>
       <div className=' p-5 flex justify-center w-[100%] h-[100%] items-center'><hr className=' p-[0.5px] w-[70%]  bg-red-100'></hr></div>
       <Projects></Projects>
+      <div className=' p-5 flex justify-center w-[100%] h-[100%] items-center'><hr className=' p-[0.5px] w-[70%]  bg-red-100'></hr></div>
+      <Contact></Contact>
     </div>
   )
 }
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,11 @@
 export default function Contact() {
     return (
         <div id='contact' className='min-h-full'>
-            ciao
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis pariatur, quibusdam tempora fugiat soluta expedita dolores vel repellat repudiandae enim ipsum alias? Iure deserunt corporis consequuntur dolores suscipit qui sapiente repudiandae facere quisquam laborum impedit, obcaecati quibusdam. Nostrum officiis ad molestias fugiat facere nemo. Vitae facere nesciunt saepe a obcaecati ducimus, quasi voluptatum, laborum sed similique ipsam incidunt illum nemo neque cumque sequi repudiandae soluta quam voluptas porro molestias officia laudantium? Doloribus nostrum laborum esse quo voluptatum, exercitationem dignissimos delectus porro magnam quia laudantium illum architecto magni quasi dolore aperiam temporibus, quibusdam voluptatibus saepe rerum tempora eius. Consequatur, neque est.
+            <div className="flex flex-col flex-1/3 justify-center items-center leading-15">
+                <span className="text-[5vh] lg:text-[8.5vh] font-[jersey] text-center tracking-widest font-bold drop-shadow-[0_0_10px_rgba(255,0,128,0.6)] text-white">
+                    Ideas become interfaces, <span className="text-red-400 underline">fast.</span>
+                </span>
+            </div>
         </div>
     )
 }
